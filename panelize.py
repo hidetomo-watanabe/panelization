f3555ca6d9f12850d1c8134cea887994c21ac1aa
hidetomo-watanabe <attyamunn@gmail.com>
2018-04-15 06:41:55 +0000

    paste回数を自動計算

--- panelize.py
+++ panelize.py
@@ -31,30 +31,48 @@ def get_random_params(CANVAS_WIDTH, CANVAS_HEIGHT):
     return width_random, height_random, rotate_random
 
 
+def calc_paste_ratio(canvas):
+    zero_count = 0
+    non_zero_count = 0
+    for count, pixel in canvas.getcolors(256**3):
+        if sum(pixel) == 0:
+            zero_count += count
+        else:
+            non_zero_count += count
+    paste_ratio = non_zero_count / (non_zero_count + zero_count)
+    return paste_ratio
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         output_filename = sys.argv[1]
     else:
         output_filename = 'tmp'
-    if len(sys.argv) > 2:
-        paste_num = int(sys.argv[2])
-    else:
-        paste_num = 1
 
     # const
     FILEDIR = './files'
     CANVAS_WIDTH = 1440
     CANVAS_HEIGHT = 900
+    MIN_PASTE_RATIO = 0.7
+    MAX_PASTE_NUM = 100
 
     ims, imwidths, imheights = get_im_data(FILEDIR)
     canvas = Image.new(
         'RGB', (CANVAS_WIDTH, CANVAS_HEIGHT), (0, 0, 0))
-    for _ in range(paste_num):
+
+    paste_count = 0
+    paste_ratio = 0
+    while (paste_ratio < MIN_PASTE_RATIO) and (paste_count < MAX_PASTE_NUM):
+        # paste
         for i in range(len(ims)):
             width_random, height_random, rotate_random = \
                 get_random_params(CANVAS_WIDTH, CANVAS_HEIGHT)
             canvas.paste(
                 ims[i].rotate(rotate_random),
                 (width_random, height_random))
+        paste_count += 1
+        paste_ratio = calc_paste_ratio(canvas)
 
+    print('[INFO] PASTE COUNT: %s' % paste_count)
+    print('[INFO] PASTE RATIO: %s' % paste_ratio)
     canvas.save('%s.jpg' % output_filename, quality=100)
